95407290c0dfb8275775cf8a013d5f9243683601
Haran Kumar <hkumar8200@gmail.com>
2017-08-20 13:07:51 -0500

    barebones basic file walker

--- src/compiler.js
+++ src/compiler.js
@@ -1,7 +1,37 @@
 "use strict";
 
-function compile(js){
-    return `fn main(){println!("Hello World from Rust!");}`;
+const acorn = require('acorn');
+
+function walk(astNode) {
+  switch (astNode.type) {
+    case "Program":
+      const body = astNode.body.map(walk).map((s) => s + ";").join("\n");
+      return `fn main(){${body}}`;
+    case "ExpressionStatement":
+      return walk(astNode.expression);
+    case "CallExpression":
+      const callee = walk(astNode.callee);
+      const args = astNode.arguments.map(walk);
+      return `${callee}(${args})`;
+    case "MemberExpression":
+      if (astNode.object.name === "console" &&
+        astNode.property.name === "log")
+        return "println!"
+    case "Literal":
+      const val = astNode.value;
+      switch (typeof val) {
+        case "string":
+          return `"${val}"`
+      }
+    default:
+      console.log(astNode);
+      console.log("IDK what to do with this!!!");
+  }
+}
+
+function compile(js) {
+  const AST = acorn.parse(js);
+  return walk(AST);
 }
 
 module.exports = compile;
