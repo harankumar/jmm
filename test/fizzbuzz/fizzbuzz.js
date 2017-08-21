0339f128e20d92fb7cea208db354a0833e382c61
Haran Kumar <hkumar8200@gmail.com>
2017-08-21 07:23:07 -0500

    Add additional FizzBuzz example

--- test/fizzbuzz/fizzbuzz.js
+++ test/fizzbuzz/fizzbuzz.js
@@ -1,3 +1,5 @@
+// Simplest Version
+console.log("Doing it the easy way!");
 for (let i = 1; i < 100; i++) {
   if (i % 15 === 0)
     console.log("FizzBuzz");
@@ -7,4 +9,17 @@ for (let i = 1; i < 100; i++) {
     console.log("Buzz");
   else
     console.log(i);
+}
+
+// Moderately More Elegant (?) Version
+console.log("Doing it the slightly less easy way!");
+for (let i = 1; i < 100; i++) {
+  var temp = "";
+  if (i % 3 == 0)
+    temp += "Fizz";
+  if (i % 5 == 0)
+    temp += "Buzz";
+  if (temp === "")
+    temp += i;
+  console.log(temp);
 }
