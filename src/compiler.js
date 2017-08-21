84ca22a36f5248398a4b8a7c2e472d6b9c84b437
Haran Kumar <hkumar8200@gmail.com>
2017-08-21 06:14:39 -0500

    Ability to compile FizzBuzz

--- src/compiler.js
+++ src/compiler.js
@@ -9,12 +9,20 @@ const walkers = {
   "VariableDeclaration": walkDeclaration,
   "VariableDeclarator": walkDeclarator,
   "Identifier": walkIdentifier,
-  "Literal": walkLiteral
+  "Literal": walkLiteral,
+  "ForStatement": walkForStatement,
+  "IfStatement": walkIfStatement,
+  "BinaryExpression": walkBinaryExpression,
+  "BlockStatement": walkBlockStatement,
+  "UpdateExpression": walkUpdateExpression,
+  "MemberExpression": walkMemberExpression
 };
+
+// TODO: de-clutter/categorize this stuff
 
 function walkProgram(astNode) {
   const body = astNode.body.map(walk).map((s) => s + ";").join("\n");
-  return `fn main(){${body}}`;
+  return `fn main(){\n${body}\n}`;
 }
 
 function walkExpression(astNode) {
@@ -29,7 +37,7 @@ function walkCall(astNode) {
   if (astNode.callee.type === "MemberExpression" &&
     astNode.callee.object.name === "console" &&
     astNode.callee.property.name === "log")
-    return `println!("{}", ${args})`
+    return `println!("{}", ${args})\n`
 
   return `${callee}(${args})`;
 }
@@ -56,7 +64,62 @@ function walkLiteral(astNode) {
   switch (typeof val) {
     case "string":
       return `"${val}"`
+    case "number":
+      return `({${val} as f64})`
   }
+}
+
+function walkForStatement(astNode) {
+  const init = walk(astNode.init);
+  const test = walk(astNode.test);
+  const update = walk(astNode.update);
+  const body = walk(astNode.body);
+
+  return `${init};\n while(${test}){\n${body}; ${update};\n}`
+}
+
+function walkIfStatement(astNode) {
+  const test = walk(astNode.test);
+  const consequent = walk(astNode.consequent);
+  const alternate = walk(astNode.alternate);
+
+  return `if(${test})\n{${consequent}} \nelse\n {${alternate}}`
+}
+
+function walkBinaryExpression(astNode) {
+  // TODO -- FIX ME!!
+  // Rust and JS don't actually have a one-to-one correlations in what operations mean
+  const left = walk(astNode.left);
+  const right = walk(astNode.right);
+  // Pull this out
+  const op = astNode.operator === "===" ? "==" : astNode.operator;
+
+  if (op === "%")
+    return `({${left}}) ${op} ({${right}})`;
+  else
+    return `${left} ${op} ${right}`;
+}
+
+function walkBlockStatement(astNode) {
+  // TODO -- FIX ME!!
+  // This probably mangles scoping rules and stuff
+
+  const body = astNode.body.map(walk).map((s) => s + ";").join("\n");
+  return body;
+}
+
+function walkUpdateExpression(astNode) {
+  // TODO -- make this complete
+  // TODO -- handle prefix
+
+  const arg = walk(astNode.argument);
+
+  if (astNode.operator === "++")
+    return `{${arg} += 1.0; ${arg}}`
+}
+
+function walkMemberExpression(astNode) {
+  // TODO
 }
 
 function walk(astNode) {
