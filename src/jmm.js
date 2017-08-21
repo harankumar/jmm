22e81dff2ebf3a2abdd1d146a996d7cdd25ddf6e
Haran Kumar <hkumar8200@gmail.com>
2017-08-21 08:12:50 -0500

    Option to call rustc by compiler or not

--- src/jmm.js
+++ src/jmm.js
@@ -6,13 +6,14 @@ const execSync = require('child_process').execSync;
 const compile = require('./compiler');
 
 const opts = cli.parse({
-  js: ["j", "A Javascript Input File", "file", "../test/helloworld/helloworld.js"],
-  out: ["o", "Output directory", "file", "../test/helloworld/build"]
+  js: ["j", "A Javascript Input File", "file", "../test/fizzbuzz/fizzbuzz.js"],
+  out: ["o", "Output directory", "file", "../test/fizzbuzz/build"],
+  rustc: ["r", "Call rustc?", true, false]
 })
 
 // TODO -- make this cross OS compatible
 const stub = opts.js.split("/").slice(-1)[0].split(".")[0];
-const rsFile = `${opts.out}/${stub}.rs`;
+const rsFile = `${opts.out}/${stub}.rs`; // Should the .js stay in between?
 const htmlFile = `${opts.out}/${stub}.html`;
 
 console.log(stub, rsFile, htmlFile)
@@ -24,5 +25,7 @@ console.log(rs);
 
 
 fs.writeFileSync(rsFile, rs);
+
 // TODO -- do this better
-execSync("rustc --target=wasm32-unknown-emscripten " + rsFile + " -o " + htmlFile)
+if (opts.rustc)
+  execSync("rustc --target=wasm32-unknown-emscripten " + rsFile + " -o " + htmlFile)
