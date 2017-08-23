c4b9be6024550dac3897af9545787cb711d472b8
Haran Kumar <hkumar8200@gmail.com>
2017-08-23 12:39:17 -0500

    Add additional fibonacci example.

--- test/fibonacci/fibonacci.js
+++ test/fibonacci/fibonacci.js
@@ -6,4 +6,19 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 
+function better_fib(n) {
+  let a = 0,
+    b = 1,
+    i = 2;
+  while (i <= n) {
+    let sum = a + b;
+    a = b;
+    b = sum;
+
+    i++;
+  }
+  return b;
+}
+
+console.log(better_fib(40));
 console.log(fib(40));
