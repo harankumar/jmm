a7081a909bcab702d8d365a73271a9034946f26b
Haran Kumar <hkumar8200@gmail.com>
2017-08-23 12:43:34 -0500

    Add fibonacci example with switch statement

--- test/fibonacci/fibonacci.js
+++ test/fibonacci/fibonacci.js
@@ -6,6 +6,17 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 
+function fib_switch(n) {
+  switch (n) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    default:
+      return fib_switch(n - 1) + fib_switch(n - 2);
+  }
+}
+
 function better_fib(n) {
   let a = 0,
     b = 1,
@@ -21,4 +32,5 @@ function better_fib(n) {
 }
 
 console.log(better_fib(40));
+console.log(fib_switch(40));
 console.log(fib(40));
