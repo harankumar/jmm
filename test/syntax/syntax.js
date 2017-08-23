3b519756388b3c395e8e9e98d077650e1fd985b4
Haran Kumar <hkumar8200@gmail.com>
2017-08-23 13:57:25 -0500

    Add test for syntax quirks and features that don't show up that often. Starting with switch statement weirdities.

---
+++ test/syntax/syntax.js
@@ -0,0 +1,14 @@
+// SWITCH STATEMENTS
+let x = 5;
+switch (x) {
+  case 5:
+  case 4:
+    console.log("It's 4 or 5");
+  case 3:
+    console.log("It's 3 or 4 or 5");
+    break;
+  case 2:
+    console.log("It's 2");
+  default:
+    console.log("Not 2, 3, 4, or 5");
+}
