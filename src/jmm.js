a192b9fbd42b7019c51c6b8088bfef7423c02280
Haran Kumar <hkumar8200@gmail.com>
2017-08-24 20:09:58 -0500

    CLI flag for each test

--- src/jmm.js
+++ src/jmm.js
@@ -6,10 +6,16 @@ const execSync = require('child_process').execSync;
 const compiler = require('./compiler');
 
 const opts = cli.parse({
-  js: ["j", "A Javascript Input File", "file", "../test/fibonacci/fibonacci.js"],
-  out: ["o", "Output directory", "file", "../test/fibonacci/build"],
+  test: ["t", "Test to run", "string", null],
+  js: ["j", "A Javascript Input File", "file", null],
+  out: ["o", "Output directory", "file", null],
   rustc: ["r", "Call rustc?", true, false]
 })
+
+if (!opts.js && !opts.out && opts.test) {
+  opts.js = `../test/${opts.test}/${opts.test}.js`;
+  opts.out = `../test/${opts.test}/build`;
+}
 
 // TODO -- make this cross OS compatible
 const stub = opts.js.split("/").slice(-1)[0].split(".")[0];
