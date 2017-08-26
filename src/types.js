915d62febc01a305f081123171f6bc9f9df4faa6
Haran Kumar <hkumar8200@gmail.com>
2017-08-26 15:03:32 -0500

    Add wrapper for calling tern synchronously and type inferring

--- src/types.js
+++ src/types.js
@@ -1,1 +1,34 @@
-const tern = require('tern');
+module.exports = {
+  infer: infer
+}
+
+const tern = new(require('tern').Server)({});
+const deasync = require('deasync');
+
+function infer(astNode) {
+  const params = {
+    query: {
+      type: "type",
+      file: "main",
+      start: astNode.start,
+      end: astNode.end
+    }
+  };
+
+  let done = false;
+  let data;
+  tern.request(params, function(err, results) {
+    if (err)
+      console.log(err);
+
+    data = results;
+    done = true;
+  })
+  deasync.loopWhile(() => !done);
+
+  return data;
+}
+
+function registerFile(src) {
+  tern.addFile("main", src);
+}
