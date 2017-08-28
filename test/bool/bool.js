21eaad5ab4dd43368281262ebcb41e368fb197e3
Haran Kumar <hkumar8200@gmail.com>
2017-08-28 08:05:04 -0500

    Add booleans test

---
+++ test/bool/bool.js
@@ -0,0 +1,30 @@
+let x = true;
+let y = false;
+let a = true;
+let b = false;
+
+console.log("You should see a bunch of true printouts!");
+
+console.log(!(x && y));
+console.log(x && x);
+console.log(!(b && x));
+console.log(!y);
+console.log(!!x);
+console.log(x || y);
+console.log(x || a);
+console.log(!(b || y));
+console.log(b == y);
+console.log(a == x);
+console.log(a != b);
+console.log(a != false);
+console.log(a == true);
+console.log(b == false);
+console.log(b != true);
+console.log(true == true);
+console.log(false != true);
+console.log(false == false);
+console.log(!false);
+console.log(true && true);
+console.log(true || true);
+console.log(!(true && false));
+console.log(true);
