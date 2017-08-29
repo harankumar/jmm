3504fafff292234b954850b5e25854b4b42d337a
Haran Kumar <hkumar8200@gmail.com>
2017-08-29 12:56:48 -0500

    Add more bool/truthy tests

--- test/bool/bool.js
+++ test/bool/bool.js
@@ -2,6 +2,10 @@ let x = true;
 let y = false;
 let a = true;
 let b = false;
+let p = "a truthy string";
+let q = ""; // falsey string
+let r = 42; // truthy number
+let s = 0; // falsey number
 
 console.log("You should see a bunch of true printouts!");
 
@@ -28,3 +32,7 @@ console.log(true && true);
 console.log(true || true);
 console.log(!(true && false));
 console.log(true);
+console.log(!(s && r));
+console.log(!(p && q));
+console.log(!!(p || q));
+console.log(!!(s || r));
