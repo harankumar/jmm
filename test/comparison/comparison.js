cfc183968156de632565bef603b646d5626c5a37
Haran Kumar <hkumar8200@gmail.com>
2017-08-30 13:27:37 -0500

    Add comparisons tests

---
+++ test/comparison/comparison.js
@@ -0,0 +1,15 @@
+console.log("You should see a bunch of true printouts");
+
+// GT/LT
+console.log(5 > 2);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 >= 2);
+console.log(5 < 9);
+console.log(5 <= 5);
+//console.log(! ("foo" > 4));
+//console.log(! (999 > "foo"));
+console.log("4" > "3");
+console.log("alpha" < "delta");
+
+// EQ
