9724eb057987efa39271cbaa631db2a517d64d5a
Haran Kumar <hkumar8200@gmail.com>
2017-08-29 19:01:29 -0500

    Reformat rust code

--- src/rslib/console.rs
+++ src/rslib/console.rs
@@ -1,9 +1,9 @@
 struct Console {}
 
 impl Console {
-    fn log <T: JmmJsObject> (&self, x: T){
+    fn log<T: JmmJsObject>(&self, x: T) {
         println!("{}", x.to_str());
     }
 }
 
-static console : Console = Console {};
+static console: Console = Console {};
--- src/rslib/jmm_js_object.rs
+++ src/rslib/jmm_js_object.rs
@@ -1,8 +1,8 @@
 trait JmmJsObject {
-    fn type_of (&self) -> String;
+    fn type_of(&self) -> String;
     fn is_truthy(&self) -> bool;
-    fn to_str (&self) -> String;
-    fn to_num (&self) -> f64;
+    fn to_str(&self) -> String;
+    fn to_num(&self) -> f64;
 }
 
 impl JmmJsObject for bool {
@@ -11,21 +11,21 @@ impl JmmJsObject for bool {
     }
 
     fn is_truthy(&self) -> bool {
-      return *self;
+        return *self;
     }
 
-    fn to_str (&self) -> String {
-      match *self {
-        true => String::from("true"),
-        false => String::from("false")
-      }
+    fn to_str(&self) -> String {
+        match *self {
+            true => String::from("true"),
+            false => String::from("false"),
+        }
     }
 
-    fn to_num (&self) -> f64 {
-      match *self {
-        true => 1.0,
-        false => 0.0
-      }
+    fn to_num(&self) -> f64 {
+        match *self {
+            true => 1.0,
+            false => 0.0,
+        }
     }
 }
 
@@ -34,20 +34,20 @@ impl JmmJsObject for String {
         return String::from("string");
     }
 
-    fn is_truthy(&self) -> bool{
-      match (*self).len() {
-        0 => false,
-        _ => true
-      }
+    fn is_truthy(&self) -> bool {
+        match (*self).len() {
+            0 => false,
+            _ => true,
+        }
     }
 
-    fn to_str (&self) -> String {
-      return (*self).clone();
+    fn to_str(&self) -> String {
+        return (*self).clone();
     }
 
     fn to_num(&self) -> f64 {
-      // TODO -- change me!
-      return -42.0;
+        // TODO -- change me!
+        return -42.0;
     }
 }
 
@@ -57,18 +57,18 @@ impl JmmJsObject for f64 {
     }
 
     fn is_truthy(&self) -> bool {
-      match *self {
-        0.0 => false,
-        _ => true
-      }
+        match *self {
+            0.0 => false,
+            _ => true,
+        }
     }
 
-    fn to_str (&self) -> String {
-      // TODO -- fully flesh this out
-      return (*self).to_string();
+    fn to_str(&self) -> String {
+        // TODO -- fully flesh this out
+        return (*self).to_string();
     }
 
     fn to_num(&self) -> f64 {
-      return *self;
+        return *self;
     }
 }
