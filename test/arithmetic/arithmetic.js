0f71382fb802eff4493fd3ab95837b3fc3dc6549
Haran Kumar <hkumar8200@gmail.com>
2017-08-29 19:02:27 -0500

    Add arithmetic (add) tests

---
+++ test/arithmetic/arithmetic.js
@@ -0,0 +1,24 @@
+// ADDITION
+// addition as addition
+console.log("You should see the first 10 natural numbers!");
+console.log(1);
+console.log(1 + 1);
+console.log(1 + true + true);
+console.log(1 + true + true + 1 + false);
+console.log(false + (false + false) + (true + 4));
+let x = 5;
+let y = true;
+let z = false;
+console.log(x + (y + z));
+console.log((x + y) + 1);
+console.log(x + 2);
+console.log((x + z) + 3);
+console.log(x + (y + y + z) + 2);
+console.log(x + (y + z + (y + z) + (y + (y + z + 1))))
+// addition as concatenation
+console.log("You should see the following: 1foo, truefoo, foofalse, foo1");
+console.log(1 + "foo");
+console.log(true + "foo");
+console.log("foo" + false);
+console.log("foo" + 1);
+console.log("foo" + "bar");
