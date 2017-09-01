472b1d2d0761bd6f02d63847669083c316b763bc
Haran Kumar <hkumar8200@gmail.com>
2017-09-01 09:29:57 -0500

    Add --all flag (doesn't work yet b/c of lack of block scoping)

--- src/jmm.js
+++ src/jmm.js
@@ -10,20 +10,35 @@ const opts = cli.parse({
     test: ["t", "Test to run", "string", null],
     js: ["j", "A Javascript Input File", "file", null],
     out: ["o", "Output directory", "file", null],
-    rustc: ["r", "Call rustc?", true, false]
+    rustc: ["r", "Call rustc?", true, false],
+    all: ["a", "Run all tests?", true, false]
 });
 
-if (!opts.js && !opts.out && opts.test) {
-    opts.js = `../test/${opts.test}/${opts.test}.js`;
-    opts.out = `../test/${opts.test}/build`;
+if (!opts.js && !opts.out) {
+    if (opts.all){
+        opts.out = `../test/all/build`
+    } else if (opts.test){
+        opts.js = `../test/${opts.test}/${opts.test}.js`;
+        opts.out = `../test/${opts.test}/build`;
+    }
 }
 
-// TODO -- make this cross OS compatible
-const stub = opts.js.split("/").slice(-1)[0].split(".")[0];
-const rsFile = `${opts.out}/${stub}.rs`; // Should the .js stay in between?
-const htmlFile = `${opts.out}/${stub}.html`;
+let program = "", rsFile, htmlFile;
 
-const program = fs.readFileSync(opts.js);
+if (opts.out){
+    // TODO -- make this cross OS compatible
+    const stub = opts.js.split("/").slice(-1)[0].split(".")[0];
+    rsFile = `${opts.out}/${stub}.rs`; // Should the .js stay in between?
+    htmlFile = `${opts.out}/${stub}.html`;
+
+    program = fs.readFileSync(opts.js);
+} else {
+    let names = fs.readdirSync("../test/");
+    for (let name of names)
+        program += fs.readFileSync(`../test/${name}/${name}.js`) + "\n\n";
+    rsFile = "../test/all/build/all.rs";
+    htmlFile = "../test/all/build/all.html";
+}
 
 const rs = compiler.compile(program);
 
