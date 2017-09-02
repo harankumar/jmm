ed69c9bdfeafc86ed9029447b1d87d37c72171dd
Haran Kumar <hkumar8200@gmail.com>
2017-09-02 10:18:24 -0500

    More complex OOP test

--- test/oop/oop.js
+++ test/oop/oop.js
@@ -1,10 +1,31 @@
-function Beep(){
-
+function Beep(msg) {
+    this.msg = msg;
+    console.log("Hello from Beep!");
 }
 
-function Foo(){
+Beep.prototype = {
+    beep: function () {
+        console.log("BEEP: " + this.msg);
+    }
+};
+
+function Foo() {
     this.a = "boo";
+    console.log("Hello from Foo!");
 }
 
-let x = new Foo();
-let y = new Beep();
+Foo.prototype = {
+    blork: function () {
+        this.a += "foo";
+    },
+    quork: function () {
+        console.log(this.a);
+    }
+};
+
+let x = new Foo(); // Hello from Foo!
+let y = new Beep("boop"); // Hello from Beep!
+y.beep(); // BEEP: boop
+x.quork(); // boo
+x.blork();
+x.quork(); // boofoo
