8a572ecc47e8a7463b18c4085dd359a8b9910e39
Haran Kumar <hkumar8200@gmail.com>
2017-09-05 12:25:02 -0500

    some basic bitwise operators tests

---
+++ test/bitwise/bitwise.js
@@ -0,0 +1,7 @@
+console.log("You should see a bunch of zeros:");
+console.log(1 >> 2);
+console.log(1.6548684 >> 2);
+console.log(6 & 1);
+console.log(0.00000 | 0);
+console.log(0 << 0.0);
+console.log(55.5 ^ 55.5);
