70a6b111183e50aab9a00163062ee400bc805249
Haran Kumar <hkumar8200@gmail.com>
2017-09-08 10:35:53 -0500

    Add Float64Array to built in classes

--- src/transform/fake_classes.js
+++ src/transform/fake_classes.js
@@ -43,7 +43,16 @@ function removeBuiltinClasses(classNames) {
         "Date",
         "RegExp",
         "Error",
-        "JSON"
+        "JSON",
+        // "Int8Array",
+        // "Uint8Array",
+        // "Uint8ClampedArray",
+        // "Int16Array",
+        // "Uint16Array",
+        // "Int32Array",
+        // "Uint32Array",
+        // "Float32Array",
+        "Float64Array"
     ]);
 
     return classNames.filter((name) => !builtin.has(name));
