4512f7c17633aac7b013ba24b2e41c2eb6756026
Haran Kumar <hkumar8200@gmail.com>
2017-09-08 09:57:08 -0500

    Add tests for arrays of bools and arrays of numbers

--- test/arrays/arrays.js
+++ test/arrays/arrays.js
@@ -4,3 +4,16 @@ x.push("baz");
 x.push("bar");
 console.log(x);
 console.log(x.length);
+
+let y = [true, false, true];
+y.push(true);
+y.push(false);
+y.push(1 === 1);
+console.log(y);
+
+let z = [1,2,3,4,5];
+z.push(1 / 33);
+z.push(1111.0202);
+z.push(12321421);
+z.push(0.0000001);
+console.log(z);
