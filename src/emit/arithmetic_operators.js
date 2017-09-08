4666bf6f6c60fa3acfee463102993edd50d6f14a
Haran Kumar <hkumar8200@gmail.com>
2017-09-08 13:49:32 -0500

    Remove type coercion overhead on arithmetic operations performed on numbers

--- src/emit/arithmetic_operators.js
+++ src/emit/arithmetic_operators.js
@@ -14,14 +14,18 @@ function isNumCoercible(type) {
 }
 
 function buildAdd(left, right, left_type, right_type) {
-    if (num_coercible.has(left_type) && num_coercible.has(right_type))
+    if (left_type === "number" && right_type === "number"){
+        return `((${left}) + (${right}))`;
+    } else if (num_coercible.has(left_type) && num_coercible.has(right_type))
         return `((${left}).to_num() + (${right}).to_num())`;
     else
         return `[(${left}).to_str(), (${right}).to_str()].join("")`;
 }
 
 function buildSub(left, right, left_type, right_type) {
-    if (num_coercible.has(left_type) && num_coercible.has(right_type))
+    if (left_type === "number" && right_type === "number"){
+        return `((${left}) - (${right}))`;
+    } else if (num_coercible.has(left_type) && num_coercible.has(right_type))
         return `((${left}).to_num() - (${right}).to_num())`;
     else
         return "__js__NaN";
@@ -29,7 +33,9 @@ function buildSub(left, right, left_type, right_type) {
 
 function buildTimes(left, right, left_type, right_type) {
     // TODO -- handle Infinity
-    if (num_coercible.has(left_type) && num_coercible.has(right_type))
+    if (left_type === "number" && right_type === "number"){
+        return `((${left}) * (${right}))`;
+    } else if (num_coercible.has(left_type) && num_coercible.has(right_type))
         return `((${left}).to_num() * (${right}).to_num())`;
     else
         return "__js__NaN";
@@ -37,14 +43,18 @@ function buildTimes(left, right, left_type, right_type) {
 
 function buildDiv(left, right, left_type, right_type) {
     // TODO -- handle Infinity
-    if (num_coercible.has(left_type) && num_coercible.has(right_type))
+    if (left_type === "number" && right_type === "number"){
+        return `((${left}) / (${right}))`;
+    } else if (num_coercible.has(left_type) && num_coercible.has(right_type))
         return `((${left}).to_num() / (${right}).to_num())`;
     else
         return "__js__NaN";
 }
 
 function buildMod(left, right, left_type, right_type){
-    if (num_coercible.has(left_type) && num_coercible.has(right_type))
+    if (left_type === "number" && right_type === "number"){
+        return `((${left}) % (${right}))`;
+    } else if (num_coercible.has(left_type) && num_coercible.has(right_type))
         return `((${left}).to_num() % (${right}).to_num())`;
     else
         return "__js__NaN";
