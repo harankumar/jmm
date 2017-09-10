1ed014329fb0e2caf65654264eb90028938dd28d
Haran Kumar <hkumar8200@gmail.com>
2017-09-10 18:59:02 -0500

    Fewer automatic casts to f64 if unneeded (~10% speedup!!!)

--- src/emit/literals.js
+++ src/emit/literals.js
@@ -11,8 +11,27 @@ function walkLiteral(astNode) {
         case "string":
             return `String::from("${val}")`;
         case "number":
-            return `({${val} as f64})`;
+            return walkNumberLiteral(val);
         case "boolean":
             return val.toString();
     }
+}
+
+function walkNumberLiteral(val) {
+    // Check if it's a double
+    // If it is, just return it
+    // If it can be converted to f64 easily, do so
+    // Otherwise, cast
+
+    val = val.toString();
+
+    let simple_double = val.match(/[0-9]+\.[0-9]*/g);
+    if (simple_double && simple_double.length === 1 && simple_double[0] === val)
+        return val;
+
+    let simple_int = val.match(/[0-9]+/g);
+    if (simple_int && simple_int.length === 1 && simple_int[0] === val)
+        return val + ".0";
+
+    return `({${val} as f64})`;
 }
