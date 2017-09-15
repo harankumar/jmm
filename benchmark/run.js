a4a7df83812aab6d995d56ec10a3a43248d39023
Haran Kumar <hkumar8200@gmail.com>
2017-09-15 08:19:29 -0500

    Add benchmark runner

---
+++ benchmark/run.js
@@ -0,0 +1,42 @@
+const Benchmark = require('benchmark');
+
+const execSync = require("child_process").execSync;
+
+const params = {
+    "fannkuch": 7, // 12
+    "spectralnorm": 5, // 5500
+    "nbody": 5 //0000000
+};
+
+for (let prog of ["fannkuch", "spectralnorm", "nbody"]) {
+    const param = params[prog];
+    const suite = new Benchmark.Suite;
+
+    suite
+        .add(prog + "-js", function () {
+            execSync(`node ./${prog}/js/src/${prog}.js ${param}`)
+        })
+        .add(prog + "-jmm-bin", function () {
+            execSync(`./${prog}/jmm/bin/${prog}_jmm ${param}`)
+        })
+        .add(prog + "-jmm-wasm", function () {
+            execSync(`cd ./${prog}/jmm/wasm/ && node ${prog}_jmm.js ${param} && cd ../../..`)
+        })
+        .add(prog + "-cpp-bin", function () {
+            execSync(`./${prog}/cpp/bin/${prog}.gpp_run ${param}`)
+        });
+
+    suite
+        .on('cycle', function (event) {
+            console.log(String(event.target));
+        })
+        .on('complete', function () {
+            console.log('Fastest is ' + this.filter('fastest').map('name'));
+        })
+        .run({
+            async: true,
+            maxTime: 1
+        });
+}
+
+
